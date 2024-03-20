4d9d84e59cb4b9ef319106c7f245974c7d192151
Saikiran Thalloju <saikiranthalloju008@gmail.com>
2024-03-20 17:08:50 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+  const [input, setInput] = useState("");
+  const [filteredArray, setFilteredArray] = useState([]);
+  const [flag, setFlag] = useState(false);
+
+  function autoComplete() {
+    const filteredFruits = fruits.filter((fruit) =>
+      fruit.toLowerCase().includes(input.toLowerCase())
+    );
+    setFilteredArray(filteredFruits);
+    setFlag(true);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      <input
+        type="text"
+        onInput={autoComplete}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      {flag &&
+        filteredArray.map((fruit, index) => (
+          <ul key={index}>
+            <li>{fruit}</li>
+          </ul>
+        ))}
+    </div>
+  );
+};
+
+export default App;
